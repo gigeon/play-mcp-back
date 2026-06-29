f3ec61a525e8d2e5e727e490038b0d9048970c55
gigeon <igigeon442@gmai.com>
2026-06-29 17:17:46 +0900

    fix: 키 값 헤더에서 가져오게 수정

--- src/main/java/play/mcp/back/tools/YouthPoilyTool.java
+++ src/main/java/play/mcp/back/tools/YouthPoilyTool.java
@@ -1,10 +1,14 @@
 package play.mcp.back.tools;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.model.ToolContext;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import play.mcp.back.common.BaseMap;
 import play.mcp.back.domain.api.service.ApiService;
 
@@ -24,10 +28,13 @@ public class YouthPoilyTool {
         lclsfNm: 참여권리/복지문화/주거/교육/일자리
     """)
     public BaseMap getYouthPoily(
-            @ToolParam(description = "Api Key(youthcenter.go.kr)") String apiKeyNm,
             @ToolParam(description = "정책대분류") String lclsfNm
     ) {
+        HttpServletRequest request = ((ServletRequestAttributes)
+                RequestContextHolder.currentRequestAttributes()).getRequest();
+        String apiKeyNm = request.getHeader("apiKeyNm");
         BaseMap param = new BaseMap();
+
         param.put("apiKeyNm", apiKeyNm);
         param.put("lclsfNm", lclsfNm);
 
